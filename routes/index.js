32846a80af46abe7e2fa688a3f265f6684edd4c4
marcSitze <marcsitze01@gmail.com>
2022-08-20 16:43:49 +0100

    use auth routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const { getIndex } = require('../controllers')
+const authRoutes = require('./auth')
 
 router.get('/', getIndex);
+router.use('/auth', authRoutes);
 
 module.exports = router;
